41ac0154b6ce028aefc0caf75aadd9f96410573d
Augustine Edeh <info.augustinesedeh@gmail.com>
2021-06-16 09:46:22 +0100

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,38 @@
+//TTS.speak("hey");
+
+
+//var e = document.getElementById("pw");
+
+
+
+
+// Show/Hide password
+function show_Password() {
+  var e = document.getElementById("pw");
+  var a = document.getElementById("sp");
+
+
+  if (e.type == "password") {
+    e.type = "text";
+    a.innerHTML = "Hide password";
+  }  else {
+    e.type = "password";
+    a.innerHTML = "Show password";
+  }  
+}
+
+
+/* practice; accessing the
+attributes of HTML elements */
+
+
+/* Check for REGEX attachment for the said code */
+
+
+
+
+
+
+//JavaScript validation codes for -
+//alpha_Numeric characters
+//And  minimum character length.
